5bf25b514571e8ff18a63d246b4de4736467357f
Krirll <balalaev20022@gmail.com>
2023-05-31 22:47:36 +0300

    set dependencies, update versions

---
+++ app/src/main/java/ru/krirll/notes/presentation/MainActivity.kt
@@ -0,0 +1,27 @@
+package ru.krirll.notes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import ru.krirll.notes.ui.theme.NotesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotesTheme {
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NotesTheme {
+
+    }
+}
