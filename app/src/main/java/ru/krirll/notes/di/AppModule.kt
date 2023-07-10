6450b6e4b42ca385a128505580762962f1a6ffc3
Krirll <balalaev20022@gmail.com>
2023-07-10 19:48:38 +0300

    changed for sqlite

--- app/src/main/java/ru/krirll/notes/di/AppModule.kt
+++ app/src/main/java/ru/krirll/notes/di/AppModule.kt
@@ -1,12 +1,11 @@
 package ru.krirll.notes.di
 
 import android.app.Application
-import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import ru.krirll.notes.data.notes_db.NotesDatabase
+import ru.krirll.notes.data.notes_db.NotesDbHelper
 import ru.krirll.notes.data.repository.NotesRepositoryImpl
 import ru.krirll.notes.domain.repository.NotesRepository
 import ru.krirll.notes.domain.use_case.DeleteNoteUseCase
@@ -22,18 +21,14 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideNotesDataBase(app: Application): NotesDatabase {
-        return Room.databaseBuilder(
-            app,
-            NotesDatabase::class.java,
-            NotesDatabase.DATABASE_NAME
-        ).build()
+    fun provideNotesDataBase(app: Application): NotesDbHelper {
+        return NotesDbHelper(app.applicationContext)
     }
 
     @Provides
     @Singleton
-    fun provideNotesRepository(db: NotesDatabase): NotesRepository {
-        return NotesRepositoryImpl(db.notesDao)
+    fun provideNotesRepository(db: NotesDbHelper): NotesRepository {
+        return NotesRepositoryImpl(db)
     }
 
     @Provides
